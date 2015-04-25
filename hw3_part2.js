32e3355351116c13c8156627defdbb0979686b07
bungotakahashi <takahasb@onid.orst.edu>
2015-04-25 13:37:25 -0700

    working on text box

--- hw3_part2.js
+++ hw3_part2.js
@@ -1,32 +1,30 @@
-var originalGitList = [];
+var originalGistList = [];
 
 //document.getElementById("submit").onclick = function() { fetchData(); };
 
-var a=function(){
-  var a = document.createElement("div");
-  a.innerHTML="haha";
-  var box = document.getElementsByTagName("box")[0];
 
-  body.appendChild(a);
-};
 
 var fetchData= function(){
 
   var req = new XMLHttpRequest();
-
+  var url = 'https://api.github.com/gists/public';
   if(!req){
     throw "Unable to create HttpRequest.";
   }
 
   req.onreadystatechange = function (){
 
-    if(this.readyState === 4){
+      
 
-        var arr = JSON.parse(this.responseText);
-        alert("aaa");
-        for (var i=0; i< arr.length; i++){
-          originalGistList[i] = arr [i];
+  
+    if((this.readyState === 4) && (this.status === 200)) {
+
+        
+        originalGistList = JSON.parse(this.responseText);
+        for (var i=0; i< originalGistList.length; i++){
+          originalGistList[i] = originalGistList [i];
           var gist = originalGistList[i];
+
           generateGistHtml(gist);
 
 
@@ -40,7 +38,7 @@ var fetchData= function(){
   };
 
 
-  req.open('GET', "https://api.github.com/gists/public");
+  req.open('GET',url);
   req.send();
 
 
@@ -49,15 +47,28 @@ var fetchData= function(){
 
 var generateGistHtml= function(gist){
 
-  var body = document.getElementsByTagName("body")[0];
-  var tbl     = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+  var box1 = document.getElementById("box1");
+  var tbl1     = document.createElement("table");
+  var tblBody1 = document.createElement("tbody");
+
+  var box2 = document.getElementById("box2");
+  var tbl2     = document.createElement("table");
+  var tblBody2 = document.createElement("tbody");
 
   var des = document.createElement("div");
-  des.innerHTML = gist.description;
+  if (gist.description ){
+    des.innerHTML = gist.description;
+  }
 
-  var url = document.createElement("href");
-  url.innerHTML = gist.url;
+  else{
+    des.innerHTML = "NO DESCRIPTION";
+  }
+
+  var url = document.createElement("a");
+  url.setAttribute("href", gist.url);
+   newText = document.createTextNode(""+gist.url);
+   url.appendChild(newText);
+  
 
 
   var fbutton = document.createElement("button"); //this fbutton code is from the sample by Mr. Ghorash.
@@ -67,26 +78,44 @@ var generateGistHtml= function(gist){
   fbutton.onclick = function(){
 	   var gistId = this.getAttribute("gistId");
 	   var toBeFavoredGist = findById(gistId);
-
+     box1.removeChild(tbl1);
+     box2.appendChild(tbl1);
+     
   };
 
-  var row = document.createElement("tr");
-  var cell = document.createElement("td");
-  var cellText = document.createTextNode("cell in row ");
-  cell.appendChild(cellText);
-  //cell.appendChild(url);
-  //cell.appendChild(fbutton);
-  row.appendChild(cell);
-  tblBody.appendChild(row);
-  tbl.appendChild(tblBody);
-  body.appendChild(tbl);
+ /* var fbutton2 = document.createElement("button"); //this fbutton code is from the sample by Mr. Ghorash.
+  fbutton.innerHTML = "-";
+  fbutton.setAttribute("gistId", gist.id);
+
+  fbutton.onclick = function(){
+     var gistId = this.getAttribute("gistId");
+     
+     box2.removeChild(tbl1);
+     box1.appendChild(tbl1);
+
+  };
+*/
+
+  var row1 = document.createElement("tr");
+  var cell1 = document.createElement("td");
+  var cellText1 = document.createTextNode("des.innerHTML");
+  cell1.appendChild(fbutton);
+  cell1.appendChild(des);
+  cell1.appendChild(url);
+
+  
+  row1.appendChild(cell1);
+  tblBody1.appendChild(row1);
+  tbl1.appendChild(tblBody1);
+  box1.appendChild(tbl1);
+  tbl1.setAttribute("border","2");
 };
 
 
 var findById = function(id) {
-	for (var i=0; i < originalGitList.length; i++){
-    if (originalGitList[i].id == id){
-      return originalGitList[i];
+	for (var i=0; i < originalGistList.length; i++){
+    if (originalGistList[i].id == id){
+      return originalGistList[i];
     }
   }
   return undefined;
