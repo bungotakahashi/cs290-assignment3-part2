9926cc399c2fd215b583f7325c5a07191f99fc71
bungotakahashi <takahasb@onid.orst.edu>
2015-04-25 23:19:18 -0700

    complete other than extra point functions

--- hw3_part2.js
+++ hw3_part2.js
@@ -1,6 +1,5 @@
 var originalGistList = [];
-
-//document.getElementById("submit").onclick = function() { fetchData(); };
+var favoriteList = [];
 
 
 
@@ -14,12 +13,7 @@ var fetchData= function(){
 
   req.onreadystatechange = function (){
 
-      
-
-  
     if((this.readyState === 4) && (this.status === 200)) {
-
-        
         originalGistList = JSON.parse(this.responseText);
         for (var i=0; i< originalGistList.length; i++){
           originalGistList[i] = originalGistList [i];
@@ -27,13 +21,9 @@ var fetchData= function(){
 
           generateGistHtml(gist);
 
-
-
         }
 
-
     }
-
 
   };
 
@@ -50,11 +40,10 @@ var generateGistHtml= function(gist){
   var box1 = document.getElementById("box1");
   var tbl1     = document.createElement("table");
   var tblBody1 = document.createElement("tbody");
+  var row1 = document.createElement("tr");
+  var cell1 = document.createElement("td");
 
-  var box2 = document.getElementById("box2");
-  var tbl2     = document.createElement("table");
-  var tblBody2 = document.createElement("tbody");
-
+  
   var des = document.createElement("div");
   if (gist.description ){
     des.innerHTML = gist.description;
@@ -71,34 +60,28 @@ var generateGistHtml= function(gist){
   
 
 
-  var fbutton = document.createElement("button"); //this fbutton code is from the sample by Mr. Ghorash.
+  var fbutton = document.createElement("button"); //this fbutton code is from the sample by Mr. Ghorashi.
   fbutton.innerHTML = "+";
   fbutton.setAttribute("gistId", gist.id);
 
   fbutton.onclick = function(){
+
+
 	   var gistId = this.getAttribute("gistId");
 	   var toBeFavoredGist = findById(gistId);
+     favoriteList.push(toBeFavoredGist);
+
      box1.removeChild(tbl1);
-     box2.appendChild(tbl1);
+     
+     
+     localStorage.clear();
+     localStorage["favoriteItems"] = JSON.stringify(favoriteList);
+      favorites(toBeFavoredGist);
+     
      
   };
 
- /* var fbutton2 = document.createElement("button"); //this fbutton code is from the sample by Mr. Ghorash.
-  fbutton.innerHTML = "-";
-  fbutton.setAttribute("gistId", gist.id);
-
-  fbutton.onclick = function(){
-     var gistId = this.getAttribute("gistId");
-     
-     box2.removeChild(tbl1);
-     box1.appendChild(tbl1);
-
-  };
-*/
-
-  var row1 = document.createElement("tr");
-  var cell1 = document.createElement("td");
-  var cellText1 = document.createTextNode("des.innerHTML");
+ 
   cell1.appendChild(fbutton);
   cell1.appendChild(des);
   cell1.appendChild(url);
@@ -108,7 +91,7 @@ var generateGistHtml= function(gist){
   tblBody1.appendChild(row1);
   tbl1.appendChild(tblBody1);
   box1.appendChild(tbl1);
-  tbl1.setAttribute("border","2");
+  tbl1.style.border = "thin solid black";
 };
 
 
@@ -118,5 +101,84 @@ var findById = function(id) {
       return originalGistList[i];
     }
   }
+
   return undefined;
 };
+
+var favorites = function(gist) {
+  
+  var box = document.getElementById("box2");
+  var tbl     = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+  var row = document.createElement("tr");
+
+  var des = document.createElement("div");
+  if (gist.description ){
+    des.innerHTML = gist.description;
+  }
+
+  else{
+    des.innerHTML = "NO DESCRIPTION";
+  }
+
+  var url = document.createElement("a");
+  url.setAttribute("href", gist.url);
+   newText = document.createTextNode(""+gist.url);
+   url.appendChild(newText);
+
+   var fbutton = document.createElement("button"); //this fbutton code is from the sample by Mr. Ghorashi.
+  fbutton.innerHTML = "-";
+  fbutton.setAttribute("gistId", gist.id);
+
+  fbutton.onclick = function(){
+    for (var i=0; i < favoriteList.length; i++){
+      if (favoriteList[i].id == gist.id){  
+        favoriteList.splice(i,1);
+      }
+     
+    }
+     box.removeChild(tbl);
+     
+     
+     localStorage.clear();
+     localStorage["favoriteItems"] = JSON.stringify(favoriteList);
+     generateGistHtml(gist);
+  };
+
+
+
+  var cell = document.createElement("td");
+
+  cell.appendChild(fbutton);
+  cell.appendChild(des);
+  cell.appendChild(url);
+
+  row.appendChild(cell);
+  tblBody.appendChild(row);
+  tbl.appendChild(tblBody);
+  box.appendChild(tbl);
+
+  
+  tbl.style.border = "thin solid black";
+  
+  
+}
+
+var load = function(){
+
+ if(JSON.parse(localStorage.getItem("favoriteItems"))){
+    alert('not empty');
+    favoriteList = JSON.parse(localStorage.getItem("favoriteItems"));
+ }
+
+ else{
+    alert("empty");
+ }
+  
+  for (var i=0; i< favoriteList.length; i++){
+    favorites(favoriteList[i]);
+  }
+
+} 
+
+window.onload=load;
